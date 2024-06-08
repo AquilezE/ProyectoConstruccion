777d8fe3d785c6de11deeddd095fc81ef3a81f41
marco1gk <zS22013628@estudiantes.uv.mx>
2024-06-08 00:13:38 -0600

    agregue en consultar solicitudes de constancia,apenas le agarre la onda a este pedo jeje

---
+++ src/proyectoconstruccion/Controllers/oferta/FXMLEditarOfertaColaboracionController.java
@@ -0,0 +1,32 @@
+package proyectoconstruccion.Controllers.oferta;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLEditarOfertaColaboracionController implements Initializable {
+
+
+    public TextField lbTitulo;
+    public TextField idIdioma;
+    public TextField IdDuracion;
+    public ComboBox idCBPeriodo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void btnCancelar(ActionEvent actionEvent) {
+    }
+
+    public void btnEditar(ActionEvent actionEvent) {
+    }
+}
+
+
