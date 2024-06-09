d0e7035724c07abc0920576454649dfc147f2d7e
marco1gk <zS22013628@estudiantes.uv.mx>
2024-06-09 15:11:20 -0600

    cambie colaboracionTerminado

--- src/proyectoconstruccion/Controllers/colaboracion/FXMLDetallesColaboracionTerminado.java
+++ src/proyectoconstruccion/Controllers/colaboracion/FXMLDetallesColaboracionTerminado.java
@@ -1,5 +1,6 @@
 package proyectoconstruccion.Controllers.colaboracion;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -9,5 +10,20 @@ public class FXMLDetallesColaboracionTerminado implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void btnRegresar(ActionEvent actionEvent) {
+    }
+
+    public void btnAprobarConstancia(ActionEvent actionEvent) {
+    }
+
+    public void btnEvidencia(ActionEvent actionEvent) {
+    }
+
+    public void btnSyllabus(ActionEvent actionEvent) {
+    }
+
+    public void btnDescargarListaEstudiantes(ActionEvent actionEvent) {
     }
 }
