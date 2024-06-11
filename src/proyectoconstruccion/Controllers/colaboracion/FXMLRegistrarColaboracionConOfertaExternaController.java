3dc7d2f1b6cff555865b5f9346a98a32008682fd
unaay <unaay@ASUS>
2024-06-10 21:05:43 -0500

    agregue labels y empece con registrar colab a partir de oferta uv

--- src/proyectoconstruccion/Controllers/colaboracion/FXMLRegistrarColaboracionConOfertaExternaController.java
+++ src/proyectoconstruccion/Controllers/colaboracion/FXMLRegistrarColaboracionConOfertaExternaController.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectoconstruccion.Controllers.colaboracion;
 
-/**
- *
- * @author unaay
- */
 public class FXMLRegistrarColaboracionConOfertaExternaController {
     
 }
--- src/proyectoconstruccion/Controllers/colaboracion/FXMLRegistrarColaboracionConOfertaUVController.java
+++ src/proyectoconstruccion/Controllers/colaboracion/FXMLRegistrarColaboracionConOfertaUVController.java
@@ -1,23 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package proyectoconstruccion.Controllers.colaboracion;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author unaay
- */
 public class FXMLRegistrarColaboracionConOfertaUVController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
